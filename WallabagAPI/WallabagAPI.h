85b4ea173755f2bb23b245843154105ce42da7cd
tylinux <tylinux@xnure.com>
2022-12-06 00:18:33 +0800

    create xcode project

---
+++ WallabagAPI/WallabagAPI.h
@@ -0,0 +1,18 @@
+//
+//  WallabagAPI.h
+//  WallabagAPI
+//
+//  Created by tylinux on 2022/10/9.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WallabagAPI.
+FOUNDATION_EXPORT double WallabagAPIVersionNumber;
+
+//! Project version string for WallabagAPI.
+FOUNDATION_EXPORT const unsigned char WallabagAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WallabagAPI/PublicHeader.h>
+
+
